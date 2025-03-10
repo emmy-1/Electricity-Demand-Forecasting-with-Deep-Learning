5fddf663e62811a2311a9a1c285921d16d688376
Emmanuel Obayomi <eobayomi2@gmail.com>
2025-03-10 19:57:04 +0000

    Doc: Updated the documentation for stored_procedure_bronze_log_error

--- scripts/Bronez_layer/logs/stored_procedure_bronze_log_error.sql
+++ scripts/Bronez_layer/logs/stored_procedure_bronze_log_error.sql
@@ -1,13 +1,25 @@
 -- ================================================================================================================================
 -- Stored Procedure: bronze.log_error
--- Purpose: 1. Logs errors that occur during the execution of the bronze.load_data procedure.
---          2. Capture detailed error information, including the error number, severity, state, procedure, line number, and message.
---          3. Store the error details in the Bronze.error_logs table for later review and analysis.
+-- Purpose: 
+-- 1. Logs errors that occur during the execution of any stored procedure or SQL script.
+-- 2. Captures detailed error information, including the error number, severity, state, procedure, line number, and message.
+-- 3. Stores the error details in the Bronze.error_logs table for later review and analysis.
 
--- Procedure Overview: 1. Captures error information using SQL Server's built-in error functions and inserts it into the Bronze.error_logs table.
---                     2. The CREATE OR ALTER statement ensures the procedure is updated if it already exists, avoiding the need to drop it manually.
+-- Procedure Overview:
+-- 1. Captures error information using SQL Server's built-in error functions and inserts it into the Bronze.error_logs table.
+-- 2. The CREATE OR ALTER statement ensures the procedure is updated if it already exists, avoiding the need to drop it manually.
 
--- Example Execution:  If an error occurs in stored_procedure_bronze_load_data, the bronze.log_error procedure will log the error details to the Bronze.error_logs table.
+-- Key Features:
+-- 1. Captures detailed error information for debugging and analysis.
+-- 2. Stores error details in the Bronze.error_logs table.
+-- 3. Idempotent design: The CREATE OR ALTER statement ensures the procedure is always up-to-date.
+
+-- Example Execution:
+-- If an error occurs in any stored procedure or script, the bronze.log_error procedure will log the error details to the Bronze.error_logs table.
+
+-- Dependencies:
+-- 1. Bronze.error_logs Table: The table must exist with the following schema:see Bronze_error_logs table schema
+-- 2. Permissions: The user executing the procedure must have INSERT permissions on the Bronze.error_logs table.
 -- ================================================================================================================================
 
 -- Code Breakdown
